e0441d531ca3785f24f823bf1560c685e1685dba
niluhpwstriii <116167670+niluhpwstriii@users.noreply.github.com>
2023-10-20 18:50:46 +0700

    Latihan1_php.php

--- Latihan1_php.php
+++ Latihan1_php.php
@@ -1,67 +1,31 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Kalkulator Sederhana</title>
+    <title>Contoh Form Bootstrap</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 </head>
 <body>
 
-<div class="container">
-    <h1 class="mt-5">Kalkulator</h1>
+<div class="container mt-5">
+    <h1>Contoh Form Bootstrap</h1>
 
     <form method="post" class="mt-4">
         <div class="form-group">
-            <input type="text" class="form-control" name="angka1" required>
+            <label for="angka">Masukkan Angka:</label>
+            <input type="number" class="form-control" name="angka" id="angka" placeholder="Angka" required>
         </div>
         <div class="form-group">
-            <select class="form-control" name="operator">
-                <option value="tambah">Tambah (+)</option>
-                <option value="kurang">Kurang (-)</option>
-                <option value="kali">Kali (x)</option>
-                <option value="bagi">Bagi (/)</option>
+            <label for="pilihan">Pilih Angka:</label>
+            <select class="form-control" name="pilihan" id="pilihan">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
             </select>
-        </div>
-        <div class="form-group">
-            <input type="text" class="form-control" name="angka2" required>
         </div>
         <button type="submit" class="btn btn-primary" name="hitung">Hitung</button>
     </form>
-
-    <?php
-    if (isset($_POST['hitung'])) {
-        $angka1 = $_POST['angka1'];
-        $angka2 = $_POST['angka2'];
-        $operator = $_POST['operator'];
-
-        if (is_numeric($angka1) && is_numeric($angka2)) {
-            switch ($operator) {
-                case 'tambah':
-                    $hasil = $angka1 + $angka2;
-                    break;
-                case 'kurang':
-                    $hasil = $angka1 - $angka2;
-                    break;
-                case 'kali':
-                    $hasil = $angka1 * $angka2;
-                    break;
-                case 'bagi':
-                    if ($angka2 != 0) {
-                        $hasil = $angka1 / $angka2;
-                    } else {
-                        $hasil = "Pembagian dengan nol tidak valid.";
-                    }
-                    break;
-                default:
-                    $hasil = "Operasi tidak valid.";
-            }
-
-            echo "<p class='mt-3'>Hasil: $hasil</p>";
-        } else {
-            echo "<p class='mt-3 text-danger'>Masukkan harus berupa angka.</p>";
-        }
-    }
-    ?>
-
 </div>
 
 </body>
